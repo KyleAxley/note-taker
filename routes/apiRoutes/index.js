74164236185c38d363fa388a67ce08a47f8f300d
KyleAxley <kanderson7645@yahoo.com>
2022-08-03 18:00:03 -0400

    deploy site

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router(); 
 const { notes } = require("../../db/db.json");
+const { v4: uuidv4 } = require('uuid'); 
+const { createNewNote } = require('../../lib/notes');
 
 //route to retrieve all notes
 router.get('/notes', (req, res) => {
@@ -12,10 +14,13 @@ router.post('/notes', (req, res) => {
     let notesList = notes 
     //validate req.body
     if(req.body.title && req.body.text) {
-        //todo set unique id 
+        //todo set unique id
+        req.body.id = uuidv4(); 
         //todo create new note 
+        const note = createNewNote(req.body, notesList); 
+        res.json(note); 
     } else {
-
+        res.status(404).send("something went wrong, these are not the drones you are looking for."); 
     } 
 })
 
